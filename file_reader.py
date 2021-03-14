41585e8c2ca6eef40f02c4f36c9bed0b7595145b
Minh Le <leminh2000@gmail.com>
2021-03-14 14:49:09 +0700

    add python src

--- file_reader.py
+++ file_reader.py
@@ -2,6 +2,7 @@ with open('pi_million_digits.txt') as file_object:
     lines=file_object.readlines()
 pi_string=''
 for line in lines:
+
     pi_string+=str(line.strip())
 your_birth_date = input("enter your birth date yyddmm: ")
 print(your_birth_date)
